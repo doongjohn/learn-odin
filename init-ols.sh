1f6e5b1a9b66bae894224bcd84b7d8aaeadf31ea
doongjohn <doongjohn@gmail.com>
2023-07-16 23:23:14 +0900

    update

--- init-ols.sh
+++ init-ols.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
+
 (echo | cat << ___
 {
+  "\$schema": "https://raw.githubusercontent.com/DanielGavin/ols/master/misc/ols.schema.json",
   "collections": [
     { "name": "core", "path": "${HOME}/odin/core" },
     { "name": "vendor", "path": "${HOME}/odin/vendor" },
   ],
-  "enable_semantic_tokens": true,
-  "enable_document_symbols": true,
   "enable_hover": true,
-  "enable_format": true,
   "enable_snippets": true,
-  "formatter": {
-    "tabs": true,
-    "characters": 100
-  }
+  "enable_semantic_tokens": true,
+  "enable_document_symbols": true
 }
 ___
 ) > ols.json
+
+(echo | cat << ___
+{
+  "\$schema": "https://raw.githubusercontent.com/DanielGavin/ols/master/misc/odinfmt.schema.json",
+  "tabs": true,
+  "tabs_width": 4,
+  "character_width": 100
+}
+___
+) > odinfmt.json

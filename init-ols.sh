c6eb81cc1ffae4635aacb6c90a5c02fb78111b59
doongjohn <doongjohn@gmail.com>
2024-02-06 21:46:20 +0900

    update

--- init-ols.sh
+++ init-ols.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
-(echo | cat << ___
+ODIN_HOME="$HOME/odin"
+
+(echo | cat << ===
 {
   "\$schema": "https://raw.githubusercontent.com/DanielGavin/ols/master/misc/ols.schema.json",
-  "collections": [
-    { "name": "core", "path": "${HOME}/odin/core" },
-    { "name": "vendor", "path": "${HOME}/odin/vendor" },
-  ],
   "enable_hover": true,
   "enable_snippets": true,
   "enable_semantic_tokens": true,
-  "enable_document_symbols": true
+  "enable_document_symbols": true,
+  "enable_inlay_hints": true,
+  "collections": [
+    { "name": "base", "path": "$ODIN_HOME/base" },
+    { "name": "core", "path": "$ODIN_HOME/core" },
+    { "name": "vendor", "path": "$ODIN_HOME/vendor" }
+  ]
 }
-___
+===
 ) > ols.json
 
-(echo | cat << ___
+(echo | cat << ===
 {
   "\$schema": "https://raw.githubusercontent.com/DanielGavin/ols/master/misc/odinfmt.schema.json",
+  "character_width": 100,
   "tabs": true,
-  "tabs_width": 4,
-  "character_width": 100
+  "tabs_width": 4
 }
-___
+===
 ) > odinfmt.json
